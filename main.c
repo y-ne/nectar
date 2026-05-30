755dbd643d483b5aed4b7acdea0e4b1995a92ebf
Wakimae <akumakumu@gmail.com>
2026-05-30 03:15:30 -0700

    fix popen pclose for linux

--- main.c
+++ main.c
@@ -6,8 +6,16 @@
 #define MAX_DEV 16
 #define MAX_SER 128
 
+#ifdef _WIN32
+#define popen _popen
+#define pclose _pclose
+#define NUL "nul"
+#else
+#define NUL "/dev/null"
+#endif
+
 int get_serials(char serials[][MAX_SER]) {
-  FILE *fp = _popen("adb devices", "r");
+  FILE *fp = popen("adb devices", "r");
   assert(fp);
 
   char line[256];
@@ -17,21 +25,27 @@ int get_serials(char serials[][MAX_SER]) {
 
   for (; fgets(line, sizeof(line), fp) && n < MAX_DEV;) {
     char *tab = strstr(line, "\tdevice");
-    if (tab)
-      *tab = '\0', strncpy(serials[n++], line, MAX_SER);
+    if (tab) {
+      *tab = '\0';
+      strncpy(serials[n], line, MAX_SER - 1)[MAX_SER - 1] = 0;
+      n++;
+    }
   }
-  _pclose(fp);
+  pclose(fp);
   return n;
 }
 
 int main() {
   char serials[MAX_DEV][MAX_SER];
-  int n, i;
-  assert(system("adb --version >nul 2>&1") == 0 && "adb not found");
-  assert(system("scrcpy --version >nul 2>&1") == 0 && "scrcpy not found");
+  char cmd[128];
 
-  n = get_serials(serials);
-  for (i = 0; i < n; i++)
+  snprintf(cmd, sizeof(cmd), "adb --version >%s 2>&1", NUL);
+  assert(system(cmd) == 0 && "adb not found");
+  snprintf(cmd, sizeof(cmd), "scrcpy --version >%s 2>&1", NUL);
+  assert(system(cmd) == 0 && "scrcpy not found");
+
+  int n = get_serials(serials);
+  for (int i = 0; i < n; i++)
     printf("%s\n", serials[i]);
   return 0;
 }
